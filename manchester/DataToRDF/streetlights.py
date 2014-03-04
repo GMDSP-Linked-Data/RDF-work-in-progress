34d0628ebed55345ec0d65f25313868fb9497fd7
Daniel Kershaw <danjamker@gmail.com>
2014-03-04 10:32:56 +0000

    Changes to street lighting parser

--- manchester/DataToRDF/streetlights.py
+++ manchester/DataToRDF/streetlights.py
@@ -39,7 +39,7 @@ from rdflib.namespace import FOAF, DC
 import csv
 import pprint
 
-storefn = os.path.dirname(os.path.realpath(__file__)) + '/allotments.n3'
+storefn = os.path.dirname(os.path.realpath(__file__)) + '/streetlight.n3'
 #storefn = '/home/simon/codes/film.dev/movies.n3'
 storeuri = 'file://'+storefn
 title = 'Movies viewed by %s'
@@ -47,10 +47,11 @@ title = 'Movies viewed by %s'
 r_who = re.compile('^(.*?) <([a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)+)>$')
 
 OS = Namespace('http://data.ordnancesurvey.co.uk/ontology/admingeo/')
+SPACIAL = Namespace('http://data.ordnancesurvey.co.uk/ontology/spatialrelations/')
 RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
 GEO = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
 VCARD = Namespace('http://www.w3.org/2006/vcard/ns#')
-
+SCHEMA = Namespace('http://schema.org/')
 class Store:
     def __init__(self):
         self.graph = Graph()
@@ -65,20 +66,23 @@ class Store:
         self.graph.bind('os', OS)
         self.graph.bind('rdfs', RDFS)
         self.graph.bind('geo', GEO)
+        self.graph.bind('schema', SCHEMA)
+        self.graph.bind('spacial', SPACIAL)
 
     def save(self):
-        print storeuri
         self.graph.serialize(storeuri, format='n3')
 
-    def new_streetlight(self, address, application, disabled_access, external_link, guidence, location, name, plot_size, rent):
-        allotment = BNode() # @@ humanize the identifier (something like #rev-$date)
-        self.graph.add((allotment, VCARD['hasstreetaddress'], Literal(address)))
-        #self.graph.add((allotment, DC['date'], Literal(application)))
-        #self.graph.add((allotment, DC['date'], Literal(disabled_access)))
+    #def new_streetlight(self, height, easting, eligible, lamp, lampwatts, location, mintyn, northing, objectId, street, unitid, unitno):
+    def new_streetlight(self, height, easting, northing, street):
+        streetlamp = BNode() # @@ humanize the identifier (something like #rev-$date)
+        self.graph.add((streetlamp, SCHEMA['height'], Literal(height)))
+        self.graph.add((streetlamp, SPACIAL['easting'], Literal(easting)))
+        self.graph.add((streetlamp, SPACIAL['northing'], Literal(northing)))
+        self.graph.add((streetlamp, VCARD['hasstreetaddress'], Literal(street)))
         #self.graph.add((allotment, DC['date'], Literal(external_link)))
         #self.graph.add((allotment, DC['date'], Literal(guidence)))
-        self.graph.add((allotment, GEO["lat//long"], Literal(location)))
-        self.graph.add((allotment, RDFS['label'], Literal(name)))
+        #self.graph.add((allotment, GEO["lat//long"], Literal(location)))
+        #self.graph.add((allotment, RDFS['label'], Literal(name)))
         #self.graph.add((allotment, DC['date'], Literal(plot_size)))
         #self.graph.add((allotment, GEO['rating'], Literal(rent)))
         self.save()
@@ -91,8 +95,8 @@ def main(argv=None):
 
     reader = csv.DictReader(open('./Data/Street_Lighting.txt', mode='r'))
     for row in reader:
-        #s.new_allotment(row["Address"], row["Application"], row["Disabled access"], row["External link"], row["Guidance"], row["Location"], row["Name"], row["Plot sizes"], row["Rent"])
         pprint.pprint(row)
+        s.new_streetlight(row["COLHEIGHT"], row["EASTING"], row["NORTHING"], row['STREET'])
 
 if __name__ == '__main__':
     main()
