f3ba718f81093e44eed60708ac3e2c61bc2d6fba
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-03-21 12:09:07 +0000

    Added facility ontology

--- salford/gmdspconverters/recycling.py
+++ salford/gmdspconverters/recycling.py
@@ -7,14 +7,15 @@ from rdflib.namespace import XSD
 
 from gmdspconverters import utils
 
-RECYCLING = Namespace('http://data.gmdsp.org.uk/id/salford/recycling-centre/')
+RECYCLING = Namespace('http://data.gmdsp.org.uk/id/salford/recyclingsites/')
+GMDSP = Namespace('http://data.gmdsp.org.uk/def/')
 
 def convert(graph, input_path):
 
     reader = csv.DictReader(open(input_path, mode='r'))
     for row in reader:
-        rc = RECYCLING[utils.idify(row["UPRN"])]
-        graph.add((rc, RDF.type, URIRef('http://data.gmdsp.org.uk/def/council/recycling-centre')))
+        rc = RECYCLING[utils.idify(row["Location"])]
+        graph.add((rc, RDF.type, URIRef('http://data.gmdsp.org.uk/def/recyclingsites')))
         graph.add((rc, utils.RDFS['label'], Literal(row["Location"])))
 
         address = utils.idify(row["Address"])
@@ -23,7 +24,7 @@ def convert(graph, input_path):
         # now add the address VCARD
         vcard = RECYCLING["address/"+address]
         graph.add((vcard, RDF.type, utils.VCARD["location"]))
-        graph.add((vcard, utils.VCARD['hasStreetAddress'], Literal(row["Address"])))
+        graph.add((vcard, utils.VCARD['street-address'], Literal(row["Address"])))
 
         # location information
         graph.add((rc, utils.OS["northing"], Literal(row["Northings"])))
@@ -31,24 +32,20 @@ def convert(graph, input_path):
 
 
         # recycling information
-        if row["Cardboard"]:
-            graph.add((rc, RECYCLING["hasCardboard"], Literal("true", datatype=XSD.boolean)))
-        if row["Paper"]:
-            graph.add((rc, RECYCLING["hasPaper"], Literal("true", datatype=XSD.boolean)))
-        if row["Cartons"]:
-            graph.add((rc, RECYCLING["hasCartons"], Literal("true", datatype=XSD.boolean)))
-        if row["Shoes"]:
-            graph.add((rc, RECYCLING["hasShoes"], Literal("true", datatype=XSD.boolean)))
-        if row["Glass"]:
-            graph.add((rc, RECYCLING["hasGlass"], Literal("true", datatype=XSD.boolean)))
-        if row["Textiles"]:
-            graph.add((rc, RECYCLING["hasTextiles"], Literal("true", datatype=XSD.boolean)))
-        if row["Cans"]:
-            graph.add((rc, RECYCLING["hasCans"], Literal("true", datatype=XSD.boolean)))
-        if row["Foil"]:
-            graph.add((rc, RECYCLING["hasFoil"], Literal("true", datatype=XSD.boolean)))
-        if row["Plastic Bottles"]:
-            graph.add((rc, RECYCLING["hasPlasticBottles"], Literal("true", datatype=XSD.boolean)))
-        if row["Aerosols"]:
-            graph.add((rc, RECYCLING["hasAerosols"], Literal("true", datatype=XSD.boolean)))
 
+        # maps the CSV header to the recycling facility concept schema
+        facility_map = {
+            "Cardboard": "Cardboard",
+            "Paper": "Paper",
+            "Cartons": "Cartons",
+            "Shoes": "Shoes",
+            "Glass": "Glass",
+            "Textiles": "Textiles",
+            "Cans": "Cans",
+            "Plastic Bottles": "Plastic",
+            "Aerosols": "Aerosols",
+        }
+
+        for facility in facility_map:
+            if row[facility]:
+                graph.add((rc, GMDSP['facility'], Literal(facility_map[facility])))
