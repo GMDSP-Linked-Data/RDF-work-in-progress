8631a1315f0ad283de48b868db2159f800353eb6
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-03-21 08:44:31 +0000

    Added dynamic loading of module

--- salford/main.py
+++ salford/main.py
@@ -23,6 +23,8 @@ For example:
 __author__ = 'jond'
 
 import argparse
+import importlib
+import gmdspconverters
 
 parser = argparse.ArgumentParser(description="Command line module for access to the GMDSP converters.")
 parser.add_argument(
@@ -37,8 +39,21 @@ parser.add_argument("-o", type=str, required=True, help="The output file.")
 
 args = parser.parse_args()
 
+
+MODULE_MAP = {
+    'allotments': 'allotments',
+    'gritting': 'gritting',
+    'parking': 'parking',
+    'planning': 'planning',
+    'recycling': 'recycling',
+    'streetlight': 'streetlight',
+}
+
 def main(convertertype, inputfile_path, outputfile_path):
-    pass
+    m = importlib.import_module(MODULE_MAP[convertertype], gmdspconverters)
+    g = gmdspconverters.utils.create_graph(outputfile_path)
+    m.convert(g, inputfile_path)
+    gmdspconverters.utils.output_graph(outputfile_path)
 
 
 if __name__ == "__main__":
