3f2b8c2159c96e8b38bdc5fd787ea35b30119310
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-03-28 10:20:16 +0000

    Now about statistical information in correct format with reference to correct ontologies

--- salford/gmdspconverters/allotmentstatistics.py
+++ salford/gmdspconverters/allotmentstatistics.py
@@ -11,6 +11,8 @@ al = Namespace('http://data.gmdsp.org.uk/id/salford/allotments/')
 al_ont = Namespace('http://data.gmdsp.org.uk/def/council/allotment/')
 al_stat = Namespace('http://gmdsp.org/def/statistical-dimension/allotments/')
 
+YEAR_STRING = "2013-Q4"
+
 
 def convert(graph, input_path):
 
@@ -21,9 +23,8 @@ def convert(graph, input_path):
 
         if row["Plots"]:
             try:
-                graph.add((allotment, al_stat["plots"], Literal(int(row["Plots"]), datatype=XSD.integer)))
+                graph.add((allotment, utils.RDFS['label'], Literal("{}, {}, number of plots".format(row["Name"], YEAR_STRING))))
+                graph.add((allotment, al_ont["numberOfPlots"], Literal(int(row["Plots"]), datatype=XSD.integer)))
                 graph.add((allotment, RDF.type, utils.QB['Observation']))
-                graph.add((allotment, utils.QB["dataSet"], Literal(al)))
-                graph.add((allotment, al_stat["statistics"], allotment))
             except ValueError:
                 pass
