face21c3f3d5c9854b396d850f98d1962fc6550e
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-06-20 13:25:56 +0100

    Updated converter to use new source data

--- salford/gmdspconverters/streetlighting.py
+++ salford/gmdspconverters/streetlighting.py
@@ -16,38 +16,38 @@ def convert(graph, input_path):
     reader = csv.DictReader(open(input_path, mode='r'))
 
     for row in reader:
-        if row["FEATURE ID"]:
-            sl = STREETLIGHT[utils.idify(row["FEATURE ID"])]
+        if row["Feature ID"]:
+            sl = STREETLIGHT[utils.idify(row["Feature ID"])]
             graph.add((sl, RDF.type, STREETLIGHT_ONT["Streetlight"]))
-            graph.add((sl, utils.RDFS['label'], Literal("Streetlight with ID " + row["FEATURE ID"])))
+            graph.add((sl, utils.RDFS['label'], Literal("Streetlight with ID " + row["Feature ID"])))
 
-            address = utils.idify(row["FEATURE ID"])
+            address = utils.idify(row["Feature ID"])
             graph.add((sl, utils.VCARD['hasAddress'], STREETLIGHT["address/"+address]))
 
             # now add the address VCARD
             vcard = STREETLIGHT["address/"+address]
             graph.add((vcard, RDF.type, utils.VCARD["Location"]))
-            graph.add((vcard, utils.RDFS['label'], Literal("Address of streetlight with ID " + row["FEATURE ID"])))
-            graph.add((vcard, utils.VCARD['street-address'], Literal(row["ROADNAME"])))
-            graph.add((vcard, utils.VCARD['postal-code'], Literal(row["POSTCODE"])))
-            graph.add((vcard, utils.POST['postcode'], URIRef(utils.convertpostcodeto_osuri(row["POSTCODE"]))))
+            graph.add((vcard, utils.RDFS['label'], Literal("Address of streetlight with ID " + row["Feature ID"])))
+            graph.add((vcard, utils.VCARD['street-address'], Literal(row["RoadName"])))
+            #graph.add((vcard, utils.VCARD['postal-code'], Literal(row["POSTCODE"])))
+            #graph.add((vcard, utils.POST['postcode'], URIRef(utils.convertpostcodeto_osuri(row["POSTCODE"]))))
 
             # location information
-            graph.add((sl, utils.OS["northing"], Literal(row["Northings"])))
-            graph.add((sl, utils.OS["easting"], Literal(row["Eastings"])))
+            graph.add((sl, utils.OS["northing"], Literal(row["Northing"])))
+            graph.add((sl, utils.OS["easting"], Literal(row["Easting"])))
             # add conversion for lat/long
-            lat_long = utils.ENtoLL84(float(row["Eastings"]), float(row["Northings"]))
+            lat_long = utils.ENtoLL84(float(row["Easting"]), float(row["Northing"]))
             graph.add((sl, utils.GEO["long"], Literal(lat_long[0])))
             graph.add((sl, utils.GEO["lat"], Literal(lat_long[1])))
 
             # street light specific stuff
-            if row["LAMP WATTAGE"]:
-                watts = re.findall('\d+', row["LAMP WATTAGE"])[0]
+            if row["Lamp Wattage"]:
+                watts = re.findall('\d+', row["Lamp Wattage"])[0]
                 graph.add((sl, STREETLIGHT_ONT['wattage'], Literal(watts)))
-            graph.add((sl, STREETLIGHT_ONT['lampType'], Literal(row["LAMP TYPE"])))
+            graph.add((sl, STREETLIGHT_ONT['lampType'], Literal(row["Lamp Type"])))
 
-            if row["Mounting Height                                  "]:
-                height = re.findall('\d+', row["Mounting Height                                  "])[0]
+            if row["Mounting Height"]:
+                height = re.findall('\d+', row["Mounting Height"])[0]
                 graph.add((sl, STREETLIGHT_ONT['columnHeight'], Literal(height)))
 
 
