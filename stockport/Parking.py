74db9159294b745fb191964608e32a5f720249d6
Matthew Coole <matthewcoole@gmail.com>
2014-10-31 12:44:09 +0000

    Utilised LCON ontology

--- stockport/Parking.py
+++ stockport/Parking.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 from rdflib import Graph, URIRef, Namespace, RDF, Literal
 from rdflib.store import VALID_STORE
 
+
 datafile = "./Data/CarParks.xml"
 
 storefn = "./Output/parking.rdf"
@@ -14,10 +15,12 @@ RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
 GEO = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
 SCHEMA = Namespace('http://schema.org/')
 SPACIAL = Namespace('http://data.ordnancesurvey.co.uk/ontology/spatialrelations/')
-VCARD = Namespace('http://www.w3.org/2006/vcard/ns#')
+#VCARD = Namespace('http://www.w3.org/2006/vcard/ns#')
+LOCN = Namespace("http://www.w3.org/ns/locn#")
 
 PARKING = Namespace('http://data.gmdsp.org.uk/id/stockport/parking/')
 PARKING_ONT = Namespace('http://data.gmdsp.org.uk/def/council/parking/')
+
 
 class Store:
     def __init__(self):
@@ -33,7 +36,7 @@ class Store:
         self.graph.bind("geo", GEO)
         self.graph.bind("schema", SCHEMA)
         self.graph.bind("spacial", SPACIAL)
-        self.graph.bind("vcard", VCARD)
+        self.graph.bind("locn", LOCN)
         self.graph.bind("parking", PARKING_ONT)
 
     def newCarPark(self, name, label, spaces, lat, long, address, postcode):
@@ -43,20 +46,48 @@ class Store:
 
         self.graph.add((carpark, PARKING_ONT["totalNumberOfSpaces"], Literal(spaces)))
 
-        self.graph.add((carpark, GEO["lat"], Literal(lat)))
-        self.graph.add((carpark, GEO["long"], Literal(long)))
 
-        self.graph.add((carpark, VCARD['hasAddress'], URIRef("http://data.gmdsp.org.uk/id/stockport/parking/address/" + name)))
-        vcard = PARKING["address/" + name]
-        self.graph.add((vcard, RDF.type, VCARD["Location"]))
-        self.graph.add((vcard, RDFS['label'], Literal(label)))
-        self.graph.add((vcard, VCARD['street-address'], Literal(address)))
-        self.graph.add((vcard, VCARD['postal-code'], Literal(postcode)))
+        # new LOCN stuff
+        location = PARKING["location/" + name]
+        self.graph.add((location, RDF.type, LOCN["Location"]))
+        self.graph.add((location, RDFS["label"], Literal("Location of " + label + " parking site")))
+
+        geometry = PARKING["geometry/" + name]
+        self.graph.add((geometry, RDF.type, LOCN["Geometry"]))
+        self.graph.add((geometry, RDFS["label"], Literal("Geometry of " + label + " parking site")))
+
+        locnaddress = PARKING["address/" + name]
+        self.graph.add((locnaddress, RDF.type, LOCN["Address"]))
+        self.graph.add((locnaddress, RDFS["label"], Literal("Address of " + label + " parking site")))
+
+        self.graph.add((location, LOCN["geometry"], URIRef("http://data.gmdsp.org.uk/id/stockport/parking/geometry/" + name)))
+        self.graph.add((location, LOCN["address"], URIRef("http://data.gmdsp.org.uk/id/stockport/parking/address/" + name)))
+
+        self.graph.add((geometry, GEO["lat"], Literal(lat)))
+        self.graph.add((geometry, GEO["long"], Literal(long)))
+
+        self.graph.add((locnaddress, LOCN['fullAddress'], Literal(address)))
+        self.graph.add((locnaddress, LOCN['postCode'], Literal(postcode)))
+
+        self.graph.add((carpark, LOCN["location"], URIRef("http://data.gmdsp.org.uk/id/stockport/parking/location/" + name)))
+
+
+        #old vcard stuff
+        #self.graph.add((carpark, GEO["lat"], Literal(lat)))
+        #self.graph.add((carpark, GEO["long"], Literal(long)))
+
+        #self.graph.add((carpark, VCARD['hasAddress'], URIRef("http://data.gmdsp.org.uk/id/stockport/parking/address/" + name)))
+        #vcard = PARKING["address/" + name]
+        #self.graph.add((vcard, RDF.type, VCARD["Location"]))
+        #self.graph.add((vcard, RDFS['label'], Literal(label)))
+        #self.graph.add((vcard, VCARD['street-address'], Literal(address)))
+        #self.graph.add((vcard, VCARD['postal-code'], Literal(postcode)))
 
 
     def save(self):
-        print "saving to \"" + storefn +"\"..."
+        print "saving to \"" + storefn + "\"..."
         self.graph.serialize(storefn, format="pretty-xml")
+
 
 def main(argv=None):
     s = Store()
@@ -69,7 +100,8 @@ def main(argv=None):
         name = label.replace(" ", "_").replace(",", "")
         address = label
         try:
-            spaces = int(carpark.find("fme:totalcapacity", namespaces={"fme": "http://www.safe.com/xml/xmltables"}).text)
+            spaces = int(
+                carpark.find("fme:totalcapacity", namespaces={"fme": "http://www.safe.com/xml/xmltables"}).text)
         except ValueError:
             spaces = 0
         lat = carpark.find("fme:lat", namespaces={"fme": "http://www.safe.com/xml/xmltables"}).text
@@ -78,6 +110,7 @@ def main(argv=None):
         s.newCarPark(name, label, spaces, lat, long, address, postcode)
 
     s.save()
+
 
 if __name__ == '__main__':
     main()
